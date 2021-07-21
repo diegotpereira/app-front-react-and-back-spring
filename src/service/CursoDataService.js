049044752ebbddb85d50106a7ff7e85c2d194ede
Diego <diegoteixeirapereira@hotmail.com>
2021-07-21 14:44:25 -0300

    criação dos arquivos de configuração javascript.

--- src/service/CursoDataService.js
+++ src/service/CursoDataService.js
@@ -1,29 +1,26 @@
-import axios from 'axios'
+import http from "../http-common";
 
-const INSTRUTOR = 'in28minutes'
-const CURSOS_API_URL = 'http://localhost:8000'
-const INSTRUTOR_API_URL =`${CURSOS_API_URL}/instrutores/${INSTRUTOR}`
 
 class CursoDataService {
 
-    recuperarTodosCursos(name) {
-        return axios.get(`${INSTRUTOR_API_URL}/cursos`);
+    recuperarTodosCursos() {
+        return http.get("/cursos");
     }
 
-    recuperarCursos(name, id) {
-        return axios.get(`${INSTRUTOR_API_URL}/cursos/${id}`);
+    recuperarCursos(id) {
+        return http.get("/cursos/${id}");
     }
 
-    deletarCurso(name, id) {
-        return axios.delete(`${INSTRUTOR_API_URL}/cursos/${id}`);
+    deletarCurso(id) {
+        return http.delete("/cursos/${id}");
     }
 
-    atualizarCurso(name, id, curso) {
-        return axios.put(`${INSTRUTOR_API_URL}/cursos/${id}`, curso);
+    atualizarCurso(id, data) {
+        return http.put("/cursos/${id}", data);
     }
 
-    criarCurso(name, curso) {
-        return axios.post(`${INSTRUTOR_API_URL}/cursos/`, curso);
+    criarCurso(data) {
+        return http.post("/cursos/", data);
     }
 }
 
